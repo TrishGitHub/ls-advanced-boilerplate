852e5707916cf67a07c5271a2cf205352834f3d0
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2018-03-26 14:03:49 +0300

    vue circle component

--- src/scripts/modules/module.js
+++ src/scripts/modules/module.js
@@ -1,1 +1,32 @@
-console.log('hello from module');
+import Vue from 'vue'
+
+Vue.component('crcl', {
+  template: '#circle',
+  props: {
+    percents: String,
+    title: String
+  },
+  methods: {
+    handleClick: function () {
+      console.log('hello!');
+    }
+  },
+  mounted() {
+    const block = this.$refs.title;
+
+    block.style.color = 'red';
+  }
+});
+
+new Vue({
+  template: "#circle-list",
+  el: "#wrapper",
+  data: {
+    items: [
+      {title: 'Frontend', percents: '50%'},
+      {title: 'Backend', percents: '70%'},
+      {title: 'Workflow', percents: '20%'}
+    ]
+  }
+});
+
